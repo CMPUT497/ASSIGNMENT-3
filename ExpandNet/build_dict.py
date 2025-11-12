bb4d325ee6360a7123dfe680df957ccd58faad1b
CHIROOTH <chiroothgirigowda@gmail.com>
2025-11-11 18:54:44 -0700

    adding to expandNet - some debugging and cleaning - including building the dictionary

---
+++ ExpandNet/build_dict.py
@@ -0,0 +1,50 @@
+import os
+import csv
+from re import I
+import spacy
+
+nlp = spacy.load("en_core_web_lg")
+
+def normalize_urdu(text):
+    return text.replace(" ", "_")
+
+def lemmatize_en(word):
+    return nlp(word)[0].lemma_.lower().replace(" ", "_")
+
+def main(): 
+    files = []
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    dict_dir = f"{root_dir}/dictionaries"
+    for fname in os.listdir(dict_dir):
+        fpath = os.path.join(dict_dir, fname)
+        if os.path.isfile(fpath):
+            if fpath.endswith(".txt"):
+                files.append(fpath)
+
+    rows = {}
+    for file_path in files:
+        with open(file_path, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if not line or " " not in line:
+                    continue
+                ur, en = line.split(" ", 1)
+                ur = normalize_urdu(ur)
+                en = lemmatize_en(en)
+                if ur not in rows:
+                    rows[ur] = []
+                if en not in rows[ur]:
+                    rows[ur].append(en)
+
+    output_file = f"{root_dir}/dictionaries/ur_dict.tsv"
+    with open(output_file, "w", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f, delimiter="\t")
+        for ur, en_list in rows.items():
+            en_str = " ".join(en_list)
+            writer.writerow([ur, en_str])
+
+    print(f"ur_dict.tsv created at {output_file}")
+
+
+if __name__ == "__main__":
+    main()
--- ExpandNet/run_translation.py
+++ ExpandNet/run_translation.py
@@ -5,59 +5,61 @@ from transformers import pipeline
 import xml.etree.ElementTree as ET
 
 
-nlp = stanza.Pipeline('ur', processors='tokenize,pos,lemma') 
-
-
-
 def preprocess_text(text):
     text = re.sub(r"-LRB-|-RRB-", "", text)
     text = re.sub(r"\s+", " ", text).strip()
     text = re.sub(r"\(\d+\)", "", text)
     return text
 
-translator = pipeline(
-    "translation",
-    model="facebook/nllb-200-distilled-600M",
-    src_lang="eng_Latn",
-    tgt_lang="urd_Arab"
-)
+def main():
+    nlp = stanza.Pipeline('ur', processors='tokenize,pos,lemma') 
+    translator = pipeline(
+        "translation",
+        model="facebook/nllb-200-distilled-600M",
+        src_lang="eng_Latn",
+        tgt_lang="urd_Arab"
+    )
 
-xml_path = "./Data/xlwsd_se13.xml"  
-tree = ET.parse(xml_path)
-root = tree.getroot()
+    xml_path = "./Data/xlwsd_se13.xml"  
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
 
-rows = []
-for text_obj in root.findall(".//sentence"):
-    sentence_id = text_obj.attrib["id"]
-    english_text = " ".join([w.text for w in text_obj.findall("./*")])
-    english_text = preprocess_text(english_text)
+    rows = []
+    for text_obj in root.findall(".//sentence"):
+        sentence_id = text_obj.attrib["id"]
+        english_text = " ".join([w.text for w in text_obj.findall("./*")])
+        english_text = preprocess_text(english_text)
 
-    ur = translator(english_text)[0]["translation_text"]
+        ur = translator(english_text)[0]["translation_text"]
 
-    doc = nlp(ur)
+        doc = nlp(ur)
 
-    tokens_list = []
-    lemmas_list = []
-    
+        tokens_list = []
+        lemmas_list = []
+        
 
-    for sent in doc.sentences:
-        for word in sent.words:
-            tokens_list.append(word.text)
-            lemmas_list.append(word.lemma)
-
-
-    translation_token = " ".join(tokens_list)
-    translation_lemma = " ".join(lemmas_list)
+        for sent in doc.sentences:
+            for word in sent.words:
+                tokens_list.append(word.text)
+                lemmas_list.append(word.lemma)
 
 
-    lemma = " ".join(english_text.lower().split())
+        translation_token = " ".join(tokens_list)
+        translation_lemma = " ".join(lemmas_list)
 
-    rows.append([sentence_id, english_text, ur, lemma, translation_token, translation_lemma])
 
-df = pd.DataFrame(rows, columns=[
-    "sentence_id", "text", "translation", "lemma", "translation_token", "translation_lemma"
-])
+        lemma = " ".join(english_text.lower().split())
 
-df.to_csv("expandnet_step1_translate_ur.out.tsv", sep="\t", index=False)
+        rows.append([sentence_id, english_text, ur, lemma, translation_token, translation_lemma])
 
-print("\n Saved expandnet_step1_translate_ur.out.tsv\n")
+    df = pd.DataFrame(rows, columns=[
+        "sentence_id", "text", "translation", "lemma", "translation_token", "translation_lemma"
+    ])
+
+    df.to_csv("expandnet_step1_translate_ur.out.tsv", sep="\t", index=False)
+
+    print("\n Saved expandnet_step1_translate_ur.out.tsv\n")
+
+
+if __name__ == "__main__":
+    main()
